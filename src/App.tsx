e531bb732ca7c20383e0adfb6feaf26c9af10ff0
tomo_x <158121497+tomo-x7@users.noreply.github.com>
2025-03-12 03:14:17 +0000

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+export function App() {
+	return (
+		<>
+			<h1>tomo-x's homepage</h1>
+			<a
+				href="https://bsky.app/profile/did:plc:qcwvyds5tixmcwkwrg3hxgxd"
+				target="_blank"
+				rel="noreferrer noopener"
+			>
+				🦋@tomo-x.win
+			</a>
+		</>
+	);
+}
